275e5889a97c1f440c23b21ae5cf41fcee65e304
James Prestwich <james@prestwi.ch>
2019-10-30 15:03:05 -0700

    Hello friends :)

---
+++ contracts/SPVLogger.sol
@@ -0,0 +1,112 @@
+pragma solidity ^0.5.10;
+
+import {ValidateSPV} from "@summa-tx/bitcoin-spv-sol/contracts/ValidateSPV.sol";
+import {BytesLib} from "@summa-tx/bitcoin-spv-sol/contracts/BytesLib.sol";
+import {BTCUtils} from "@summa-tx/bitcoin-spv-sol/contracts/BTCUtils.sol";
+
+contract SPVLogger {
+
+    event JamesPaid(bytes32 txid);
+    event WhyJamesNotPaid(bytes32 txid, uint8 errorCode);
+
+    bytes public constant JAMES_HASH = hex"";
+
+    uint8 public constant ERR_BAD_PROOF = 1;
+    uint8 public constant ERR_BAD_VIN = 2;
+    uint8 public constant ERR_BAD_VOUT = 3;
+    uint8 public constant ERR_MUST_PAY_JAMES_MORE = 254;
+    uint8 public constant ERR_DOES_NOT_PAY_JAMES = 255;
+
+    function paysJames(
+            bytes calldata _header,
+            bytes calldata _merkleProof,
+            bytes4 _version,
+            bytes4 _locktime,
+            uint256 _indexInBlock,
+            uint8 _paysJamesIndex,
+            bytes calldata _vin,
+            bytes calldata _vout
+    ) external returns (bool) {
+        bytes32 _txid = BTCUtils.hash256(abi.encodePacked(_version, _vin, _vout, _locktime));
+
+        bool _included = _checkInclusion(
+            _header,
+            _merkleProof,
+            _indexInBlock,
+            _txid);
+
+        if (!_included) {
+            emit WhyJamesNotPaid(_txid, ERR_BAD_PROOF);
+            return false;
+        }
+
+        // Goal: use BTCUtils to validate the _vin
+        bool _validVin = false;
+        if (!_validVin) {
+            emit WhyJamesNotPaid(_txid, ERR_BAD_VIN);
+            return false;
+        }
+
+        // Goal: use BTCUtils to validate the _vout
+        bool _validVout = false;
+        if (_validVout) {
+            emit WhyJamesNotPaid(_txid, ERR_BAD_VOUT);
+            return false;
+        }
+
+        // Goal: use BTCUtils to extract the output from the vin
+        bytes memory _output = hex"";
+
+        bool _shouldPayJames = _paysJames(_output);
+        if (!_shouldPayJames) {
+            emit WhyJamesNotPaid(_txid, ERR_DOES_NOT_PAY_JAMES);
+            return false;
+        }
+
+        uint64 _payJamesSatoshis = _howMuchPayJames(_output);
+        if (_payJamesSatoshis < 100000) {
+            emit WhyJamesNotPaid(_txid, ERR_MUST_PAY_JAMES_MORE);
+            return false;
+        }
+
+        emit JamesPaid(_txid);
+        return true;
+    }
+
+    function _checkInclusion(
+        bytes memory _header,
+        bytes memory _merkleProof,
+        uint256 _indexInBlock,
+        bytes32 _txid
+    ) internal pure returns (bool) {
+        // Goal: check transaction inclusion in the header
+
+        // You'll need to extract the merkle root from the header
+        // Use BTCUtils to extract it, and BytesLib to make it a bytes32
+        // Then use ValidateSPV to check the proof
+        return false;
+    }
+
+    function _paysJames(
+        bytes memory _output
+    ) internal pure returns (bool) {
+        // Goal: Check that it pays James :)
+        //
+        // 2 steps:
+        // 1. check that it is a witness PKH output
+        // 2. Check that the output hash is equal to the JAMES_HASH variable
+        //
+        // Hint: Solidity doesn't do equality between bytes
+        // Use keccak256 to make bytes32 from bytes
+        return false;
+    }
+
+    function _howMuchPayJames(
+        bytes memory _output
+    ) internal pure returns (uint64) {
+        // Goal: Extract the value from the output
+        //
+        // Hint: BTCUtils makes this easy
+        return 0;
+    }
+}
