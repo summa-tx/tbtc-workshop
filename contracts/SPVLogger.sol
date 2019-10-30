0d5724334356296283f1201872cc7631b7874e12
James Prestwich <james@prestwi.ch>
2019-10-30 15:51:46 -0700

    it compiles

--- contracts/SPVLogger.sol
+++ contracts/SPVLogger.sol
@@ -9,27 +9,26 @@ contract SPVLogger {
     event JamesPaid(bytes32 txid);
     event WhyJamesNotPaid(bytes32 txid, uint8 errorCode);
 
-    bytes public constant PAY_JAMES = hex"";
+    bytes public constant JAMES_HASH = hex"";
 
     uint8 public constant ERR_BAD_PROOF = 1;
     uint8 public constant ERR_BAD_VIN = 2;
     uint8 public constant ERR_BAD_VOUT = 3;
-    uint8 public constant ERR_PAY_JAMES_MORE = 254;
+    uint8 public constant ERR_MUST_PAY_JAMES_MORE = 254;
     uint8 public constant ERR_DOES_NOT_PAY_JAMES = 255;
 
     function paysJames(
-            bytes4 _version,
-            bytes calldata _vin,
-            bytes calldata _vout,
-            bytes4 _locktime,
             bytes calldata _header,
             bytes calldata _merkleProof,
+            bytes4 _version,
+            bytes4 _locktime,
             uint256 _indexInBlock,
-            uint8 _paysJamesIndex
-    ) external pure returns (bool) {
-        bytes _tx = abi.encodePacked(_version, _vin, _vout, _locktime);
-        bytes32 _txid = BTCUtils.hash256(_tx);
-
+            uint8 _paysJamesIndex,
+            bytes calldata _vin,
+            bytes calldata _vout
+    ) external returns (bool) {
+        bytes32 _txid = BTCUtils.hash256(abi.encodePacked(_version, _vin, _vout, _locktime));
+        
         bool _included = _checkInclusion(
             _header,
             _merkleProof,
@@ -41,29 +40,32 @@ contract SPVLogger {
             return false;
         }
 
-        if (!BTCUtils.validateVin(_vin)) {
+        // Goal: use BTCUtils to validate the _vin
+        bool _validVin = BTCUtils.validateVin(_vin);
+        if (!_validVin) {
             emit WhyJamesNotPaid(_txid, ERR_BAD_VIN);
             return false;
         }
 
-        if (!BTCUtils.validateVout(_vout)) {
+        // Goal: use BTCUtils to validate the _vout
+        bool _validVout = BTCUtils.validateVout(_vout);
+        if (_validVout) {
             emit WhyJamesNotPaid(_txid, ERR_BAD_VOUT);
             return false;
         }
 
-        bytes memory _output = BTCUtils.extractOutputAtIndex(_paysJamesIndex);
+        // Goal: use BTCUtils to extract the output from the vin
+        bytes memory _output = BTCUtils.extractOutputAtIndex(_vin, _paysJamesIndex);
 
-        bool _paysJames =
-
-        if (!_paysJames) {
+        bool _shouldPayJames = _paysJames(_output);
+        if (!_shouldPayJames) {
             emit WhyJamesNotPaid(_txid, ERR_DOES_NOT_PAY_JAMES);
             return false;
         }
 
         uint64 _payJamesSatoshis = _howMuchPayJames(_output);
-
         if (_payJamesSatoshis < 100000) {
-            emit WhyJamesNotPaid(_txid, ERR_PAY_JAMES_MORE);
+            emit WhyJamesNotPaid(_txid, ERR_MUST_PAY_JAMES_MORE);
             return false;
         }
 
@@ -77,9 +79,18 @@ contract SPVLogger {
         uint256 _indexInBlock,
         bytes32 _txid
     ) internal pure returns (bool) {
+        // Goal: check transaction inclusion in the header
+
+        // You'll need to extract the merkle root from the header
+        // Use BTCUtils to extract it, and BytesLib to make it a bytes32
+        // Then use ValidateSPV to check the proof
+
+        bytes memory _root = BTCUtils.extractMerkleRootLE(_header);
+        bytes32 _merkleRoot = BytesLib.toBytes32(_root);
+
         return ValidateSPV.prove(
             _txid,
-            _header.extractMerkleRootLE().toBytes32(),
+            _merkleRoot,
             _merkleProof,
             _indexInBlock);
     }
@@ -87,14 +98,29 @@ contract SPVLogger {
     function _paysJames(
         bytes memory _output
     ) internal pure returns (bool) {
-        bytes _whoItPays = BTCUtils.extractPayload(_output);
+        // Goal: Check that it pays James :)
+        //
+        // 2 steps:
+        // 1. check that it is a witness PKH output
+        // 2. Check that the output hash is equal to the JAMES_HASH variable
+        //
+        // Hint: Solidity doesn't do equality between bytes
+        // Use keccak256 to make bytes32 from bytes
 
-        return keccak256(_whoItPays) == keccak256(PAY_JAMES);
+        if (keccak256(BytesLib.slice(_output, 9, 3)) != keccak256(hex"160014")) {
+            return false;
+        }
+
+        bytes memory _whoItPays = BTCUtils.extractHash(_output);
+        return keccak256(_whoItPays) == keccak256(JAMES_HASH);
     }
 
     function _howMuchPayJames(
         bytes memory _output
     ) internal pure returns (uint64) {
-
+        // Goal: Extract the value from the output
+        //
+        // Hint: BTCUtils makes this easy
+        return BTCUtils.extractValue(_output);
     }
 }
